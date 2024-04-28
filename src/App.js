4a71f9653f83666f44c9966c25543c794dbb84b0
Kurtis Emerson <kuemerson@gmail.com>
2024-04-28 14:04:54 -0500

    Added Game Elements

--- src/App.js
+++ src/App.js
@@ -1,44 +1,97 @@
-export default function Board() {
-  function Square({ value }) {
-    return (
-      <button key={value} className="square">
-        {value}
-      </button>
-    );
-  }
+import { useState } from "react";
 
-  // const board = [
-  //   [1, 2, 3],
-  //   [4, 5, 6],
-  //   [7, 8, 9],
-  // ];
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
 
-  function buildGrid() {
-    const grid = [];
-    const max = 3;
+// const board = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+// ];
 
-    for (let i = 0; i < max; i++) {
-      const row = [];
-      for (let j = 0; j < max; j++) {
-        row.push(i * max + j + 1);
-      }
-      grid.push(row);
+function buildGrid() {
+  const grid = [];
+  const max = 3;
+
+  for (let i = 0; i < max; i++) {
+    const row = [];
+    for (let j = 0; j < max; j++) {
+      row.push(i * max + j);
     }
-
-    return grid;
+    grid.push(row);
   }
 
+  return grid;
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default function Board() {
   const board = buildGrid();
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  // console.log(squares);
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
+  console.log(winner);
+  let status;
+
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Player: " + (xIsNext ? "X" : "O");
+  }
+
+  function handleClick(key) {
+    //guard clause to check for square with value
+    if (squares[key] || calculateWinner(squares)) return;
+
+    const nextSquares = squares.slice();
+    nextSquares[key] = xIsNext ? "X" : "O";
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
 
   return (
-    <div>
+    <>
+      <div className="status">{status}</div>
       {board.map((row, i) => (
         <div key={i} className="board-row">
           {row.map((square) => (
-            <Square value={square} />
+            <Square
+              key={square}
+              value={squares[square]}
+              onSquareClick={() => {
+                // console.log(square);
+                handleClick(square);
+              }}
+            />
           ))}
         </div>
       ))}
-    </div>
+    </>
   );
 }
